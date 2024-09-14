c12140af9aaaad44c8470a4ac254c595277eee34
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-13 22:10:51 -0600

    feat: se crea esquema para el enfriamiento

---
+++ src/models/cooler.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require('mongoose');
+
+const coolerSchema = new Schema({
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    type: { type: String, enum: ['Air', 'Liquid'], required: true },
+    fan_size_mm: { type: Number, required: true },
+    noise_level: { type: Number, required: true },
+    compatibility: {
+        cpu_socket: { type: String, required: true }, // Compatibilidad con el socket del CPU
+        case_clearanse: { type: Boolean, required: true } // Verifica si cabe en el gabinete
+
+    },
+});
+
+module.exports = model('cooler', coolerSchema);
