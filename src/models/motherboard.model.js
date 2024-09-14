03f7a2b02466e60dc7e7d5d59e3ebee8c379c718
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-13 22:11:50 -0600

    feat: se crea esquema para la motherboard

---
+++ src/models/motherboard.model.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const motherboardSchema = new Schema({
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    socket: { type: String, required: true },
+    compatibility: {
+        ram_type: { type: String, enum: ['DDR4', 'DDR5'], required: true },
+        pci_version: { type: String, enum: ['PCIe 3.0', 'PCIe 4.0'], required: true },
+        cpu_compatibility: { type: String, enum: ['Intel', 'AMD'], required: true },
+        gpu_compatibility: { type: String, enum: ['NVIDIA', 'AMD'], required: true },
+        storage_compatibility: {
+            sata: { type: Boolean, required: true },
+            nvme: { type: Boolean, required: true }
+        },
+        psu_power: { type: Number, required: true }
+    }
+});
+
+module.exports = model('motherboard', motherboardSchema);
