0d0223525934728298f71e1a32390824ebe04463
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-13 22:10:21 -0600

    feat: se crea esquema para los gabinetes

---
+++ src/models/case.model.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+
+const caseSchema = new Schema({
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    form_factor: { type: String, enum: ['ATX', 'Micro-ATX', 'Mini-ITX'], required: true },
+    max_gpu_length_mm: { type: Number, required: true },
+    max_cooler_height_mm: { type: Number, required: true }
+});
+
+module.exports = model('case', caseSchema);
