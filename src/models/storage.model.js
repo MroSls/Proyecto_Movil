d58e733e6941c0288decf538831f13978f3fc8f7
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-13 22:12:56 -0600

    feat: se crea esquema para el almacenamiento

---
+++ src/models/storage.model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const storageSchema = new Schema({
+    name: { type: String, required: true },
+    brand: { type: String, required: true },
+    type: { type: String,  enum: ['SATA', 'NVMe'], required: true },
+    capacity: { type: Number, required: true },
+    speed: { type: Number, required: true },
+    compatibility: {
+        motherboard_storage_type: { type: String, enum: ['SATA', 'NVMe'], required: true }
+    }
+});
+
+module.exports = model('storage', storageSchema);
