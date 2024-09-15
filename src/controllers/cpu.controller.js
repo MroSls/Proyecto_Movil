66977a6a6d1361e25697b0395c5bccac02167d36
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-15 13:46:04 -0600

    feat: se agrego el controlador para los procesadores

---
+++ src/controllers/cpu.controller.js
@@ -0,0 +1,47 @@
+const CPUService = require('../services/cpu.service');
+
+const getCPU = async (req, res) => {
+    try {
+        const cpus = await CPUService.getCPU();
+        res.json({ cpus });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const addCPU = async (req, res) => {
+    try {
+        const newCPU = await CPUService.addCPU(req.body);
+        res.status(201).json({ newCPU });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const updateCPU = async (req, res) => {
+    try {
+        const updatedCPU = await CPUService.updateCPU(req.body);
+        if (!updatedCPU) {
+            throw new Error('No se ha encontrado el procesador');
+        }
+
+        res.status(201).json({ updatedCPU });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+const deleteCPU = async (req, res) => {
+    try {
+        const deletedCPU = await CPUService.updateCPU(req.params.id);
+        if (!deletedCPU) {
+            throw new Error('No se ha encontrado el procesador');
+        }
+
+        res.status(201).json({ deletedCPU });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
+
+module.exports = { getCPU, addCPU, updateCPU, deleteCPU };
