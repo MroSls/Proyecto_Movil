34af9ee999e29cef8e732e6800a1e6f0aab43940
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-15 13:46:53 -0600

    feat: se agregaron las rutas para los procesadores

---
+++ src/routes/cpu.route.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { validateJWT } = require('../middleware/auth');
+const { getCPU, addCPU, updateCPU, deleteCPU } = require('../controllers/cpu.controller');
+
+router.get('api/cpu', validateJWT, getCPU);
+router.post('api/cpu/register', validateJWT, addCPU);
+router.put('api/cpu/update', validateJWT, updateCPU);
+router.delete('api/cpu/delete', validateJWT, deleteCPU);
+
+module.exports = router;
