3921dfa4a54c7e73f7f03506cc12081682d2dc3d
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-14 22:04:05 -0600

    feat: se agrega la funcion que valida la firma generada entre peticiones

--- src/routes/assembledPC.route.js
+++ src/routes/assembledPC.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const { validateJWT } = require('../middleware/auth');
 const { getAssembledPC, addAssembledPC, updateAssembledPC, deletedAssembledPC } = require('../controllers/assembledPC.controller');
 
-router.get('/api/assembledPC', getAssembledPC);
-router.post('/api/assembledPC/register', addAssembledPC);
-router.put('/api/assembledPC/update', updateAssembledPC);
-router.delete('/api/assembledPC/delete/:id', deletedAssembledPC);
+router.get('/api/assembledPC', validateJWT, getAssembledPC);
+router.post('/api/assembledPC/register', validateJWT, addAssembledPC);
+router.put('/api/assembledPC/update', validateJWT, updateAssembledPC);
+router.delete('/api/assembledPC/delete/:id', validateJWT, deletedAssembledPC);
 
 module.exports = router;
