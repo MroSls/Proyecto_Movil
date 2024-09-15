4a4e845cc4034023da1d8f404422b52613bd5ad8
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-14 22:04:21 -0600

    feat: se agrega la funcion que valida la firma generada entre peticiones

--- src/routes/case.route.js
+++ src/routes/case.route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const { validateJWT } = require('../middleware/auth');
 const { getCase, addCase, updateCase, deleteCase } = require('../controllers/case.controller');
 
-router.get('/api/case', getCase);
-router.post('/api/case/register', addCase);
-router.put('/api/case/update', updateCase);
-router.delete('/api/case/delete/:id', deleteCase);
+router.get('/api/case', validateJWT, getCase);
+router.post('/api/case/register', validateJWT, addCase);
+router.put('/api/case/update', validateJWT, updateCase);
+router.delete('/api/case/delete/:id', validateJWT, deleteCase);
 
 module.exports = router;
