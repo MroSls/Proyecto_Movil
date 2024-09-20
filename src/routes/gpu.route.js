b4e5a3d7b469367e6849bd5a897bb6d45ed3bb4a
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-20 13:07:29 -0600

    fix: se corrigio ruta de gpu

--- src/routes/gpu.route.js
+++ src/routes/gpu.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { validateJWT } = require('../middleware/auth');
 const { getGPU, addGPU, updateGPU, deleteGPU } = require('../controllers/gpu.controller');
 
-router.get('/api/cpu', validateJWT, getGPU);
-router.post('/api/cpu/register', validateJWT, addGPU);
-router.put('/api/cpu/update', validateJWT, updateGPU);
-router.delete('/api/cpu/delete', validateJWT, deleteGPU);
+router.get('/api/gpu', validateJWT, getGPU);
+router.post('/api/gpu/register', validateJWT, addGPU);
+router.put('/api/gpu/update', validateJWT, updateGPU);
+router.delete('/api/gpu/delete', validateJWT, deleteGPU);
 
 module.exports = router;
