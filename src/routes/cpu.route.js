605a8f4830f75708bdda2d31681137e23b1c222e
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-20 23:09:09 -0600

    fix: se corrige error que no permitia consumir la API

--- src/routes/cpu.route.js
+++ src/routes/cpu.route.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const { validateJWT } = require('../middleware/auth');
 const { getCPU, addCPU, updateCPU, deleteCPU } = require('../controllers/cpu.controller');
 
-router.get('api/cpu', validateJWT, getCPU);
-router.post('api/cpu/register', validateJWT, addCPU);
-router.put('api/cpu/update', validateJWT, updateCPU);
-router.delete('api/cpu/delete', validateJWT, deleteCPU);
+router.get('/api/cpu', validateJWT, getCPU);
+router.post('/api/cpu/register', validateJWT, addCPU);
+router.put('/api/cpu/update', validateJWT, updateCPU);
+router.delete('/api/cpu/delete', validateJWT, deleteCPU);
 
 module.exports = router;
