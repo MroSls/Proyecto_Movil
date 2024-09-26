299b642d50adfd189862239c42acbfeb76f37821
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-26 11:59:07 -0600

    feat: se agrega viewModel para los gabinetes

---
+++ lib/viewModel/case.viewModel.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../data/services/case.service.dart';
+import '../models/case.model.dart';
+
+class CaseViewModel extends ChangeNotifier {
+  final CaseService _service = CaseService();
+  List<Case> _cases = [];
+  bool _isLoading = false;
+
+  List<Case> get cases => _cases;
+  bool get isLoading => _isLoading;
+
+  Future<void> fetchCases() async {
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      _cases = await _service.fetchCases();
+    } catch (e) {
+      throw Exception('Error al cargar los gabinetes: $e');
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+}
