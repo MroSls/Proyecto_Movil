c496c084b8e779978828e10436ddb152fa243304
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-26 11:57:33 -0600

    feat: se agrega servicio para tarjetaas madres

---
+++ lib/data/services/motherboard.service.dart
@@ -0,0 +1,6 @@
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'package:proyecto_movil/data/auth/auth.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import '../../models/motherboard.model.dart';
+
