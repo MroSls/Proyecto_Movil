3033b81591b7fe31da576f5827f94331d5749807
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-26 16:15:43 -0600

    feat: se agrega servicio para motherboard

--- lib/data/services/motherboard.service.dart
+++ lib/data/services/motherboard.service.dart
@@ -1,6 +1,32 @@
-// import 'dart:convert';
-// import 'package:http/http.dart' as http;
-// import 'package:proyecto_movil/data/auth/auth.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-// import '../../models/motherboard.model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:proyecto_movil/data/auth/auth.dart';
+import '../../models/motherboard.model.dart';
 
+class MotherboardService {
+  final String endpoint = 'https://proyecto-movil-6fkk.onrender.com/api/motherboard';
+  final AuthToken authToken = AuthToken();
+
+  Future<List<Motherboard>> fetchMotherboard() async {
+    String? token = await authToken.fetchToken();
+    final response = await http.get(
+      Uri.parse(endpoint), 
+      headers: {
+      'Authorization': token ?? '',
+    },
+  );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseJson = jsonDecode(response.body);
+
+      if (responseJson['cases'] is List) {
+        List<dynamic> data = responseJson['cases'];
+        return data.map((item) => Motherboard.fromJson(item)).toList();
+      } else {
+        throw Exception('El campo "cases" no es una lista.');
+      }
+    } else {
+      throw Exception('Error al cargar los gabinetes. ${response.statusCode} => ${response.body}');
+    }
+  }
+}
