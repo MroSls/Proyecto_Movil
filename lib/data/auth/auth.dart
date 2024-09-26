feb0d02da46056ce22adaf57bd348c61337dfeb6
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-26 11:55:18 -0600

    refactor: se corrigio el nombre de la clase

--- lib/data/auth/auth.dart
+++ lib/data/auth/auth.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-class AuthService {
+class AuthToken {
   final String endpoint = 'https://proyecto-movil-6fkk.onrender.com/api/API-RESTful/auth';
 
   Future<String?> fetchToken() async {
