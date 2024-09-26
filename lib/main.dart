25b9a89b460e684871aedbc61081259db735cd7c
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-25 20:10:59 -0600

    style: se agrego formaato de codigo y un punto

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:proyecto_movil/models/assembledPC.model.dart';
+import 'package:proyecto_movil/viewModel/assembledPC.viewModel.dart';
 import 'view/home.dart';
-import 'view/builds.dart';
+import 'view/assembledPC.view.dart';
 import 'view/components.dart';
 import 'view/buildyourpc.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:provider/provider.dart';
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  void initState() {
-    super.initState();
-// Inicializar en initState
-  }
-
-  Future<List<AssembledPC>> _getAssembledPCs() async {
-    final response = await http.get(Uri.parse("http://192.168.56.1:3000/api/assembledPC"));
-
-    if (response.statusCode == 200) {
-      Map<String, dynamic> jsonResponse = jsonDecode(response.body);
-
-      if (jsonResponse.containsKey('assembledPCs')) {
-        List<dynamic> pcList = jsonResponse['assembledPCs'];
-        List<AssembledPC> assembledPCs = pcList.map((pc) => AssembledPC.fromJson(pc)).toList();
-        for (var pc in pcList) {
-          print(pc);
-        }
-        return assembledPCs;
-        // return pcList.map((pc) => AssembledPC.fromJson(pc)).toList();
-      } else {
-        throw Exception("Clave 'assembledPCs' no encontrada en la respuesta.");
-      }
-    } else {
-      throw Exception("Fallo la conexion");
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const HomePage(),
-      routes: {
-        '/builds': (context) => const Builds(),
-        '/components': (context) => const Components(
-          imagePath: '',
-          title: '',
-          description: '',
-        ),
-        '/buildyourpc': (context) => const BuildYourPC(),
-      },
+    return ChangeNotifierProvider(
+      create: (_) => AssembledPCViewModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+        routes: {
+          '/assembledPC.view': (context) => const AssembledPCView(),
+          '/components': (context) => const Components(
+            imagePath: '',
+            title: '',
+            description: '',
+          ),
+          '/buildyourpc': (context) => const BuildYourPC(),
+        },
+      ),
     );
   }
 }
