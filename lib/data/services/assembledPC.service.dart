0f1a8f083118f7960199bef65f03bbceca2f6c30
Emmanuel Guzman <guzmanjrpro@gmail.com>
2024-09-26 11:56:20 -0600

    fix: se corrigio error al obtener el token

--- lib/data/services/assembledPC.service.dart
+++ lib/data/services/assembledPC.service.dart
@@ -1,20 +1,14 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:proyecto_movil/data/auth/auth.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../../models/assembledPC.model.dart';
 
 class AssembledPCService {
   final String endpoint = 'https://proyecto-movil-6fkk.onrender.com/api/assembledPC';
-  final AuthService authService = AuthService();
-
-  Future<String?> _getToken() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('auth_token');
-  }
+  final AuthToken authToken = AuthToken();
 
   Future<List<AssembledPC>> fetchAssembledPCs() async {
-    final token = await _getToken();
+    String? token = await authToken.fetchToken();
     final response = await http.get(
       Uri.parse(endpoint),
       headers: {
@@ -33,18 +27,18 @@ class AssembledPCService {
       }
     } else {
       throw Exception(
-          'Error al cargar las PCs ensambladas: ${response.statusCode}');
+          'Error al cargar las PCs ensambladas: ${response.statusCode} => ${response.body}');
     }
   }
 
   Future<AssembledPC> getAssembledPCById(String id) async {
-    final token = await _getToken();
+    String? token = await authToken.fetchToken();
     final response = await http.get(
-      Uri.parse('$endpoint/$id'),
+      Uri.parse('$endpoint/$id'), 
       headers: {
-        'Authorization': '$token',
-      },
-    );
+        'Authorization': token ?? '',
+    },
+  );
 
     if (response.statusCode == 200) {
       return AssembledPC.fromJson(jsonDecode(response.body));
