90f705317bd4bbc3d2f3a329783c7c142769a949
Guzman Dynastie <guzmanjrpro@gmail.com>
2024-10-03 16:25:39 -0600

    feat: se agrega carrusel, nuevo estilo y optimmizacion

--- lib/view/assembledPC.view.dart
+++ lib/view/assembledPC.view.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import '../models/assembledPC.model.dart';
 import '../viewModel/assembledPC.viewModel.dart';
 import 'components.dart';
 
 class AssembledPCView extends StatefulWidget {
-
   const AssembledPCView({super.key});
 
   @override
@@ -28,16 +28,16 @@ class AssembledPCViewState extends State<AssembledPCView> {
       },
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'Builds',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          backgroundColor: Colors.blueGrey[800],
+          title: textCustom('Builds', 40, FontWeight.bold, Colors.white),
           centerTitle: true,
+          leading: IconButton( 
+            icon: Icon(Icons.arrow_back_ios_new_rounded, size: 30),
+            color: Colors.white,
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
         ),
         body: Consumer<AssembledPCViewModel>(
           builder: (context, viewModel, child) {
@@ -54,53 +54,92 @@ class AssembledPCViewState extends State<AssembledPCView> {
               itemCount: viewModel.assembledPCs.length,
               itemBuilder: (context, index) {
                 final assembledPC = viewModel.assembledPCs[index];
-                
+
                 return GestureDetector(
                   onTap: () => _navigateToDetail(assembledPC),
-                  child: Card(
-                    elevation: 5,
-                    color: Colors.grey[850], // contenedor texto pc
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius:
-                              const BorderRadius.vertical(top: Radius.circular(10)),
-                          child: Image.network(
-                            assembledPC.urlImage.isNotEmpty ? assembledPC.urlImage[0] : '',
-                            width: double.infinity,
-                            height: 400,
-                            fit: BoxFit.cover,
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
                           ),
+                        ],
+                      ),
+                      child: Card(
+                        elevation: 5,
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          side: BorderSide(color: Colors.blueGrey, width: 2),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                assembledPC.namePC,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+                              child: CarouselSlider(
+                                items: assembledPC.urlImage.isNotEmpty
+                                    ? assembledPC.urlImage.map((image) {
+                                        return Builder(
+                                          builder: (BuildContext context) {
+                                            return Image.network(
+                                              image,
+                                              width: double.infinity,
+                                              height: 400,
+                                              fit: BoxFit.cover,
+                                            );
+                                          },
+                                        );
+                                      }).toList()
+                                    : [],
+                                options: CarouselOptions(
+                                  height: 400,
+                                  aspectRatio: 16 / 9,
+                                  viewportFraction: 0.8,
+                                  initialPage: 0,
+                                  enableInfiniteScroll: true,
+                                  reverse: false,
+                                  autoPlay: true,
+                                  autoPlayInterval: Duration(seconds: 5),
+                                  autoPlayAnimationDuration: Duration(milliseconds: 1000),
+                                  autoPlayCurve: Curves.fastOutSlowIn,
+                                  enlargeCenterPage: true,
+                                  enlargeFactor: 2.0,
+                                  scrollDirection: Axis.horizontal,
                                 ),
                               ),
-                              const SizedBox(height: 10),
-                              Text(
-                                assembledPC.software,
-                                style: const TextStyle(
-                                  fontSize: 14,
-                                  color: Colors.white70,
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    textCustom(assembledPC.namePC, 18, FontWeight.bold, Colors.blueGrey),
+                                    const SizedBox(height: 10),
+                                    Row(
+                                      children: [
+                                        textCustom(assembledPC.software, 18, FontWeight.bold, Colors.grey),
+                                        Spacer(),
+                                        textCustom('\$${assembledPC.price.toDouble()} mxn', 26, FontWeight.bold, Colors.blueGrey),
+                                      ],
+                                    ),
+                                  ],
                                 ),
                               ),
-                            ],
-                          ),
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                 );
@@ -112,8 +151,14 @@ class AssembledPCViewState extends State<AssembledPCView> {
     );
   }
 
-  void _navigateToDetail(AssembledPC assembledPC) {
+  Text textCustom(String text, int size, FontWeight fontWeight, Color color) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: size.toDouble(), fontWeight: fontWeight, color: color),
+    );
+  }
 
+  void _navigateToDetail(AssembledPC assembledPC) {
     final List<Map<String, String>> componentDetails = [
       {'label': 'Wifi', 'details': assembledPC.wifi ? 'Cuenta con antena WiFi' : 'No cuenta con antena WiFi'},
       {'label': 'Color', 'details': assembledPC.color},
@@ -124,15 +169,14 @@ class AssembledPCViewState extends State<AssembledPCView> {
       {'label': 'CPU', 'details': 'Tiene un procesador ${assembledPC.cpu.cpuFamily} ${assembledPC.cpu.cpuModel} de ${assembledPC.cpu.cpuCores} nucleos con ${assembledPC.cpu.cpu} de frecuencia hasta un maximo de ${assembledPC.cpu.cpuTurboFrecuency}'},
       {'label': 'GPU', 'details': 'Tiene una tarjeta ${assembledPC.gpu.gpuName} modelo ${assembledPC.gpu.gpuDiscreteModel}'},
     ];
-  
+
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => Components(
-          imagePath: assembledPC.urlImage.isNotEmpty ? assembledPC.urlImage[0] : '',
           title: assembledPC.namePC,
-          description: assembledPC.software,
           componentDetails: componentDetails,
+          url: assembledPC.url,
         ),
       ),
     );
--- lib/view/components.dart
+++ lib/view/components.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Components extends StatelessWidget {
-  final String imagePath;
   final String title;
-  final String description;
   final List<Map<String, String>> componentDetails;
+  final String url;
 
   const Components({
-    required this.imagePath,
     required this.title,
-    required this.description,
     required this.componentDetails,
+    required this.url,
     super.key,
   });
 
@@ -18,7 +17,7 @@ class Components extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.blueGrey[800],
         title: Text(
           title,
           style: const TextStyle(
@@ -29,57 +28,110 @@ class Components extends StatelessWidget {
         ),
         centerTitle: true,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          icon: const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white, size: 30),
           onPressed: () {
-            Navigator.pop(context); // Regresa a la pantalla anterior
+            Navigator.pop(context);
           },
         ),
       ),
-      body: ListView(
+      body: Padding(
         padding: const EdgeInsets.all(10.0),
-        children: componentDetails.map((component) {
-          return Container(
-            margin: const EdgeInsets.only(bottom: 10), // Espacio entre las etiquetas
-            padding: const EdgeInsets.all(0),
-            decoration: BoxDecoration(
-              color: Colors.grey[850],
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  spreadRadius: 2,
-                  blurRadius: 4,
-                  offset: const Offset(0, 2), // Cambiar la posición de la sombra
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1.21,
                 ),
-              ],
-            ),
-            child: ExpansionTile(
-              backgroundColor: Colors.grey[850],
-              collapsedIconColor: Colors.white,
-              iconColor: Colors.white,
-              title: Text(
-                component['label']!,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+                itemCount: componentDetails.length,
+                itemBuilder: (context, index) {
+                  final component = componentDetails[index];
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[700],
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.3),
+                          spreadRadius: 2,
+                          blurRadius: 4,
+                          offset: const Offset(0, 2),
+                        ),
+                      ],
+                    ),
+                    child: ExpansionTile(
+                      collapsedIconColor: Colors.white,
+                      iconColor: Colors.white,
+                      backgroundColor: Colors.blue[700],
+                      title: Text(
+                        component['label']!,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Text(
+                            component['details']!,
+                            style: const TextStyle(
+                              fontSize: 15.6,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
               ),
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Text(
-                    component['details']!,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: Colors.white70,
+              const SizedBox(height: 60),
+              Container(
+                width: 300,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blueGrey[800],
+                    padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () async {
+                    final Uri uri = Uri.parse(url);
+                    try {
+                      if (await canLaunchUrl(uri)) {
+                        await launchUrl(uri);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text('No se pudo abrir $uri')),
+                        );
+                      }
+                    } catch (e) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Error: $e')),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Visitar la pagina del producto!',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-              ],
-            ),
-          );
-        }).toList(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
